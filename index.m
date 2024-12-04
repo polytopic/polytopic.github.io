ebbae9e08f795bab6aa99fc0ba1573181ecf6354
polytopic <polytopicdev@gmail.com>
2024-12-03 20:52:49 -0500

    Create index.m

---
+++ index.m
@@ -0,0 +1,16 @@
+# Privacy Policy
+Effective Date: 3 December 2024
+1. Introduction
+Welcome to Stone Skipper VR. We respect your privacy and are committed to protecting your personal data. This privacy policy will inform you about how we handle your data when you use our game.
+2. Data Collection
+We do not collect any personal data from users. Our game is designed to be played offline, and no information is transmitted to our servers or third parties.
+3. Data Usage
+Since we do not collect any personal data, we do not use, share, or sell any personal information.
+4. Third-Party Services
+Our game does not integrate with any third-party services that collect data.
+5. Changes to This Privacy Policy
+We may update this privacy policy from time to time. Any changes will be posted on this page with an updated effective date.
+6. Contact Us
+If you have any questions or concerns about this privacy policy, please contact us at polytopicdev@gmail.com.
+
+
